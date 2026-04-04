bb21d9d3c37ebefaefb9c07123f87551d2be8134
stimpank316-prtf <stimpank316@gmail.com>
2026-04-04 22:34:41 +0300

    Create cstring.h
    cool lib yyyyyllolll

---
+++ C|C++/c-containers/include/cstring.h
@@ -0,0 +1,24 @@
+// cstring.h, lightweight "safe" strings (made by 1 hour)
+
+#include <string.h>
+#include <stdlib.h>
+
+struct string_t {
+    char* data;
+    size_t size;
+};
+
+typedef struct string_t string;
+
+string string_create_blank();
+string string_create_from(const char* ch, int len);
+
+void string_destroy(string* str);
+
+size_t string_size(string* str);
+size_t string_get_raw_size(string* str);
+
+void string_fix(string* str);
+void string_set(string* str, const char* literal);
+void string_copy(string* source, string* destination);
+void string_swap(string* str1, string* str2);
