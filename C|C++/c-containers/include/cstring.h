5745abbda375b1b573fcbcf47d0bc1cd52d1916f
stimpank316-prtf <stimpank316@gmail.com>
2026-04-04 23:20:45 +0300

    Update cstring.h

--- C|C++/c-containers/include/cstring.h
+++ C|C++/c-containers/include/cstring.h
@@ -1,5 +1,5 @@
 // cstring.h, lightweight "safe" strings (made by 1 hour)
-// #pramga once to ensure header file is included only once (alternative to include-guards)
+// #pragma once to ensure header file is included only once (alternative to include-guards)
 #pragma once 
 
 #include <string.h>
